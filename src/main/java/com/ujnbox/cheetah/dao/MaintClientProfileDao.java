55a1f63280130fffee637c9022d42b02e1340690
mline1900 <508425827@qq.com>
2024-05-30 20:16:07 +0800

    feature:补充dao层

---
+++ src/main/java/com/ujnbox/cheetah/dao/MaintClientProfileDao.java
@@ -0,0 +1,63 @@
+package com.ujnbox.cheetah.dao;
+
+import com.ujnbox.cheetah.model.dox.MaintClientProfileDo;
+import org.apache.ibatis.annotations.*;
+
+@Mapper
+public interface MaintClientProfileDao {
+    @Insert("""
+            INSERT INTO maint_client_profile(client_name, phone_number, unit, room, building_id)
+            VALUES(#{clientName}, #{phoneNumber}, #{room}, #{buildingId})
+            """)
+    int insert(@Param("clientName") String clientName,
+               @Param("phoneNumber") String phoneNumber,
+               @Param("room") String room,
+               @Param("buildingId") Integer buildingId);
+
+    @Update("""
+            UPDATE maint_client_profile
+            SET client_name=#{clientName}
+            WHERE id=#{id}
+            """)
+    int updateClientNameById(@Param("clientName") String clientName,
+                             @Param("id") Integer id);
+
+    @Update("""
+            UPDATE maint_client_profile
+            SET unit=#{unit}
+            WHERE id=#{id}
+            """)
+    int updateUnitById(@Param("unit") String unit,
+                       @Param("id") Integer id);
+
+    @Update("""
+            UPDATE maint_client_profile
+            SET room=#{room}
+            WHERE id=#{id}
+            """)
+    int updateRoomById(@Param("room") String room,
+                       @Param("id") Integer id);
+
+    @Update("""
+            UPDATE maint_client_profile
+            SET building_id=#{buildingId}
+            WHERE id=#{id}
+            """)
+    int updateBuildingIdById(@Param("buildingId") Integer buildingId,
+                             @Param("id") Integer id);
+
+    @Update("""
+            UPDATE maint_client_profile
+            SET status=#{status}
+            WHERE id=#{id}
+            """)
+    int updateStatusById(@Param("status") Integer status,
+                         @Param("id") Integer id);
+
+    @Select("""
+            SELECT id, client_name, phone_number, unit, room, building_id, create_time, update_time, status
+            FROM maint_client_profile
+            WHERE id=#{id}
+            """)
+    MaintClientProfileDo getById(@Param("id") Integer id);
+}
---
+++ src/main/java/com/ujnbox/cheetah/dao/MaintRecordDao.java
@@ -0,0 +1,79 @@
+package com.ujnbox.cheetah.dao;
+
+import com.ujnbox.cheetah.model.dox.MaintRecordDo;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+@Mapper
+public interface MaintRecordDao {
+    @Insert("""
+            INSERT INTO maint_record(client_id, adder_id, maint_type, maint_description)
+            VALUES(#{clientId}, #{adderId}, #{maintType}, #{maintDescription})
+            """)
+    int insert(@Param("clientId") Integer clientId,
+               @Param("adderId") Integer adderId,
+               @Param("maintType") Integer maintType,
+               @Param("maintDescription") String maintDescription);
+
+    @Update("""
+            UPDATE maint_record
+            SET maint_description=#{maintDescription}
+            WHERE id=#{id}
+            """)
+    int updateMaintDescriptionById(@Param("maintDescription") String maintDescription,
+                                   @Param("id") Integer id);
+
+    @Update("""
+            UPDATE maint_record
+            SET location_description=#{locationDescription}
+            WHERE id=#{id}
+            """)
+    int updateLocationDescriptionById(@Param("locationDescription") String locationDescription,
+                                      @Param("id") Integer id);
+
+    @Update("""
+            UPDATE maint_record
+            SET maint_type=#{maintType}
+            WHERE id=#{id}
+            """)
+    int updateTypeById(@Param("maintType") Integer maintType,
+                       @Param("id") Integer id);
+
+    @Update("""
+            UPDATE maint_record
+            SET status=#{status}
+            WHERE id=#{id}
+            """)
+    int updateStatusById(@Param("status") Integer status,
+                         @Param("id") Integer id);
+
+    @Update("""
+            UPDATE maint_record
+            SET state=#{state}
+            WHERE id=#{id}
+            """)
+    int updateStateById(@Param("state") Integer state,
+                        @Param("id") Integer id);
+
+    @Update("""
+            UPDATE maint_record
+            SET reporter_id=#{reporterId}
+            WHERE id=#{id}
+            """)
+    int updateReporterIdById(@Param("reporterId") Integer reporterId,
+                             @Param("id") Integer id);
+
+    @Select("""
+            SELECT id, client_id, adder_id, reporter_id, maint_type, maint_description, location_description, status, create_time, update_time, finish_time, state
+            FROM maint_record
+            WHERE status=#{status}
+            """)
+    List<MaintRecordDo> getByStatus(@Param("status") Integer status);
+
+    @Select("""
+            SELECT id, client_id, adder_id, reporter_id, maint_type, maint_description, location_description, status, create_time, update_time, finish_time, state
+            FROM maint_record
+            """)
+    List<MaintRecordDo> getAllRecordInfo();
+}
